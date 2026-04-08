172ea3b7e93bcadd710e90f9cb3b019504890f56
christian-alexander <christian082233@gmail.com>
2026-04-08 11:51:25 +0700

    feat: view only login page

---
+++ resources/views/layout/main_layout.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link href="img/logo/logo.png" rel="icon">
+  <title>Happy Puppy Inventory</title>
+  <link href="{{ url('ruang-admin/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+  <link href="{{ url('ruang-admin/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+  <link href="{{ url('ruang-admin/css/ruang-admin.min.css') }}" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-login">
+  <!-- Login Content -->
+  <script src="{{ url('ruang-admin/vendor/jquery/jquery.min.js') }}"></script>
+  <script src="{{ url('ruang-admin/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ url('ruang-admin/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+  <script src="{{ url('ruang-admin/js/ruang-admin.min.js') }}"></script>
+
+  @yield('content')
+
+</body>
+
+</html>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,36 @@
+@extends('layout.main_layout')
+
+@section('content')
+  <!-- Login Content -->
+  <div class="container-login">
+    <div class="row justify-content-center">
+      <div class="col-xl-4 col-lg-3 col-md-6">
+        <div class="card shadow-sm my-5">
+          <div class="card-body p-0">
+            <div class="row">
+              <div class="col-lg-12">
+                <div class="login-form">
+                  <div class="text-center">
+                    <h1 class="h4 text-gray-900 mb-4">Login</h1>
+                  </div>
+                  <form class="user">
+                    <div class="form-group">
+                      <input type="email" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp"
+                        placeholder="Email">
+                    </div>
+                    <div class="form-group">
+                      <input type="password" class="form-control" id="exampleInputPassword" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                      <a href="index.html" class="btn btn-primary btn-block">Login</a>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
