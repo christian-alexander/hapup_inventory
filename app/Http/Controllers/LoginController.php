5569cff3b9f151d1b3e7291f61b88df7e5e831c8
christian-alexander <christian082233@gmail.com>
2026-04-08 12:55:33 +0700

    feat: logout

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -53,4 +53,12 @@ class LoginController extends Controller
         $response['msg'] = 'Login Gagal, harap periksa email dan password';
         return response()->json($response);
     }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/login');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use App\Http\Controllers\LoginController;
 
 Route::get('/login', [LoginController::class, 'index']);
 Route::post('/login', [LoginController::class, 'authenticate']);
+Route::get('/logout', [LoginController::class, 'logout']);
